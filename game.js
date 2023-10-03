6dd8223c11b3e4847d7ab02a0f93dd9be447ed37
Jakob <shavinski.jakob@gmail.com>
2023-10-03 16:29:16 -0700

    Create game board with table cells with event listeners to be changed on click

--- game.js
+++ game.js
@@ -1,13 +1,10 @@
-// going to want a game board, array of arrays 
-/**
- * We are going to want to dynamically create rows and columns for a board;
- *
- * Each cell of the board will have coordinates so that we know which board
- * piece to fill;
- *
- * The initial game board array of arrays will be filled with all nulls;
- */
-import GameBoard from "./gameBoard";
+"use strict";
+
+import GameBoard from "./gameBoard.js";
+
+
+const gameBoard = new GameBoard();
+gameBoard.createBoard();
 
 
 
--- gameBoard.js
+++ gameBoard.js
@@ -1,10 +1,34 @@
-class GameBoard {
+"use strict";
+
+const boardContainer = document.querySelector(".board-container");
+
+class board {
     constructor() {
-        this.board = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null]]
+        this.height = 3;
+        this.width = 3;
+    }
+
+    createBoard() {
+        const board = document.createElement('table')
+        board.className = 'game-board'
+
+        for (let x = 0; x < this.width; x++) {
+            const tr = document.createElement('tr');
+
+            for (let y = 0; y < this.height; y++) {
+                const td = document.createElement('td');
+                td.className = `${x}-${y}`;
+                td.addEventListener('click', () => {
+                    td.style.background = 'red'
+                })
+                tr.appendChild(td);
+            }
+
+            board.appendChild(tr);
+        }
+
+        boardContainer.appendChild(board);
     }
 }
 
-export default GameBoard;
+export default board;
