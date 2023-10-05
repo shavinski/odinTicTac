59f550ea82df932b4c7a1bd66e9a36c2f368d47a
Jakob <shavinski.jakob@gmail.com>
2023-10-04 17:45:46 -0700

    Add win/tie logic

--- gameController.js
+++ gameController.js
@@ -9,25 +9,71 @@ class GameController {
     /**
      * Responsible for switching active player after 
      */
-
     switchActivePlayer() {
         this.activePlayer = this.activePlayer === 'playerOne' ? 'playerTwo' : 'playerOne';
     }
 
     /**
-     * Determines which player has won the current game of tic tac toe 
+     * Determines which player has won the current game of tic tac toe or if its a tie
      * 
      * @param {Array} board - Board represents an array of arrays which is the game board representing where player pieces are located
      * 
      */
-
     determineWinOrTie(board) {
-        console.log('determineWinOrTie ran once')
-        if (board[0][0] === '❌' && board[0][1] === '❌' && board[0][2] === '❌') {
-            this.winner = 'Player One';
-            console.log('winner:', this.winner)
+
+        for (let i = 0; i < 3; i++) {
+            if (
+                board[i][0] !== null &&
+                board[i][0] === board[i][1] &&
+                board[i][1] === board[i][2]
+            ) {
+                this.winner = board[i][0] === '❌' ? 'Player One' : 'Player Two';
+                console.log(this.winner);
+                return;
+            }
+
+            if (
+                board[0][i] !== null &&
+                board[0][i] === board[1][i] &&
+                board[1][i] === board[2][i]
+            ) {
+                this.winner = board[0][i] === '❌' ? 'Player One' : 'Player Two';
+                console.log(this.winner);
+
+                return;
+            }
+        }
+
+        if (
+            board[0][0] !== null &&
+            board[0][0] === board[1][1] &&
+            board[1][1] === board[2][2]
+        ) {
+            this.winner = board[0][0] === '❌' ? 'Player One' : 'Player Two';
+            console.log(this.winner);
+
             return;
         }
+
+        if (
+            board[0][2] !== null &&
+            board[0][2] === board[1][1] &&
+            board[1][1] === board[2][0]
+        ) {
+            this.winner = board[0][2] === '❌' ? 'Player One' : 'Player Two';
+            console.log(this.winner);
+
+            return;
+        }
+
+        // Check for a tie (if the board is full)
+        if (board.flat().every((cell) => cell !== null)) {
+            this.winner = "Tie";
+            console.log(this.winner);
+
+            return;
+        }
+
     }
 }
 
--- player.js
+++ player.js
@@ -13,14 +13,9 @@ class Player {
      * @param {HTMLElement} td - The HTML element for the individual squares where a player is able to place a piece 
      * @param {Object} gameController  - The game controller object responsible for keeping track of active player and winner
      * @param {Array} board - Board represents an array of arrays which is the game board representing where player pieces are located
-     * @returns 
-     * Returns true
-     *      if the piece has been successfully placed
+     * @returns {boolean} - Returns `true` if the piece is successfully placed; otherwise, returns nothing.
      * 
-     * If a player places a piece on an spot that has a piece already
-     *      return nothing in order to exit function
      */
-
     placePiece(td, gameController, board) {
         if (td.innerHTML !== "") {
             console.log('Silly goose this box is taken!')
