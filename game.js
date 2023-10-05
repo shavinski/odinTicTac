8ff391ac4388dd0404249a3665e3fa8c0c5c4396
Jakob <shavinski.jakob@gmail.com>
2023-10-04 17:16:39 -0700

    Add docstrings for GameController methods
    Docstring for switchActivePlayer
    Docstring for determineWinOrTie

--- game.js
+++ game.js
@@ -16,8 +16,5 @@ const gameController = new GameController();
 const playerOne = new Player('Player One', '❌');
 const playerTwo = new Player('Player Two', '⭕');
 
-// Pass in the container that holds the board, pass in gameController object to
-// have access to the active player, pass in playerOne and playerTwo in order to 
-// call the logic for placeing a piece on the board
 board.createBoard(boardContainer, gameController, playerOne, playerTwo);
 
--- gameController.js
+++ gameController.js
@@ -6,15 +6,27 @@ class GameController {
         this.winner = null;
     }
 
+    /**
+     * Responsible for switching active player after 
+     */
+
     switchActivePlayer() {
         this.activePlayer = this.activePlayer === 'playerOne' ? 'playerTwo' : 'playerOne';
     }
+
+    /**
+     * Determines which player has won the current game of tic tac toe 
+     * 
+     * @param {Array} board - Board represents an array of arrays which is the game board representing where player pieces are located
+     * 
+     */
 
     determineWinOrTie(board) {
         console.log('determineWinOrTie ran once')
         if (board[0][0] === '❌' && board[0][1] === '❌' && board[0][2] === '❌') {
             this.winner = 'Player One';
             console.log('winner:', this.winner)
+            return;
         }
     }
 }
